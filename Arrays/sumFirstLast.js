709ee173fb9e949559b85644e1ffb8090d7970ca
Zhivko Shopov <shopovzh@gmail.com>
2024-10-05 11:29:52 +0100

    Find the sum of first and last array elements

--- Arrays/sumFirstLast.js
+++ Arrays/sumFirstLast.js
@@ -1,1 +1,8 @@
-console.log("Hello Softuni!");
+function sumFirstLast(array) {
+  let first = array[0];
+  let last = array[array.length - 1];
+  let sum = first + last;
+  console.log(sum);
+}
+
+sumFirstLast([20, 30, 40]);
